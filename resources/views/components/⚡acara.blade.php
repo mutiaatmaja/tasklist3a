578ce92eddd6f53bcfd7258a78eb5e0b77fe6eb7
mutiaatmaja <mutiaatmaja80@guru.smk.belajar.id>
2025-11-19 13:48:31 +0700

    form

--- resources/views/components/⚡acara.blade.php
+++ resources/views/components/⚡acara.blade.php
@@ -11,6 +11,15 @@ new class extends Component {
     <h1>Selamat Datang di Pencatatan Acara Saya</h1>
     <hr color="red" />
     <h3>Silahkan Masukkan Acara Anda</h3>
+    <label>Nama Kegiatan :</label>
+    <input type="text" wire:model='nama_kegiatan' />
+    <label>Status</label>
+    <select wire:model='status'>
+        <option value="">--pilih status</option>
+        <option value="BARU">BARU</option>
+        <option value="SELESAI">SELESAI</option>
+    </select>
+
     <hr color="blue" />
     <h3>Berikut Adalah Daftar Kegiatan Anda</h3>
     <hr color="green" />
