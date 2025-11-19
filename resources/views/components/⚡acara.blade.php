49f8f5a39010de5c2c2d766f5473b4cde15fd663
mutiaatmaja <mutiaatmaja80@guru.smk.belajar.id>
2025-11-19 13:39:35 +0700

    Update

---
+++ resources/views/components/⚡acara.blade.php
@@ -0,0 +1,17 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component {
+    //
+};
+?>
+
+<div>
+    <h1>Selamat Datang di Pencatatan Acara Saya</h1>
+    <hr color="red" />
+    <h3>Silahkan Masukkan Acara Anda</h3>
+    <hr color="blue" />
+    <h3>Berikut Adalah Daftar Kegiatan Anda</h3>
+    <hr color="green" />
+</div>
